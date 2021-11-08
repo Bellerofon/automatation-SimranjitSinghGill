cbb6808e5577c8da8897ce7af45ef38d688fb02c
Singh <Singh.89@hotmail.com>
2021-11-08 09:24:43 +0100

    elements

--- cypress/pages/IndexPages.js
+++ cypress/pages/IndexPages.js
@@ -1,10 +1,14 @@
 /// <reference types="cypress"/>
 
+// Elements
+
 const loginPageTitle = 'Testers Hotel'
 const usernameTextField = 'nth-child(1) > input'
 const passwordTextField = 'nth-child(2) > input'
 const submitBtn = '.btn'
 const formLabel = 'Login'
+
+// Actions
 
 function checkLogingPageTitle(cy){
     cy.title().should('eq', loginPageTitle)
@@ -24,18 +28,3 @@ module.exports = {
     performValidLogin
 }
 
-/*describe("Login", () => {
-    it("Login user", () =>{
-      cy.visit('http://localhost:3000')
-      cy.get("h2").should("have.text","Login") // Correct page
-      cy.get(":nth-child(1) > input").type("tester01") // input for username
-      cy.get(":nth-child(2) > input").type("GteteqbQQgSr88SwNExUQv2ydb7xuf8c") // input for passowrd
-      cy.get(".btn").click()
-      cy.get("h2").should("have.text", "Tester Hotel Overview") // verify page
-      cy.get(".username").should("contain", "Welcome tester01!") // verify who is logged in
-      cy.get(":nth-child(4) > .btn").click() // clicks reservation button
-      cy.wait(500)
-       cy.get("h2 > div").should("contain", "Reservation") // verify page
-  
-  })
- })*/
